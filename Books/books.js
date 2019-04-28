ea1142911d6367164e3f26b8897f90f4be1677f5
Katherine <krosenk729@gmail.com>
2019-04-28 18:49:37 -0400

    Create books.js

---
+++ Books/books.js
@@ -0,0 +1,452 @@
+[
+  {
+    "title": "A Confederacy of Dunces"
+  },
+  {
+    "title": "Tuesdays with Morrie"
+  },
+  {
+    "title": "The Design of Everyday Things"
+  },
+  {
+    "title": "How to Lie with Statistics"
+  },
+  {
+    "title": "Start with Why: How Great Leaders Inspire Everyone to Take Action. by Simon Sinek"
+  },
+  {
+    "title": "Love Does"
+  },
+  {
+    "title": "Beautiful Ruins"
+  },
+  {
+    "title": "The Stranger in the Woods"
+  },
+  {
+    "title": "Black Edge: Inside Information, Dirty Money, and the Quest to Bring Down the Most Wanted Man on Wall Street"
+  },
+  {
+    "title": "Cold Tangerines: Celebrating the Extraordinary Nature of Everyday Life"
+  },
+  {
+    "title": "Finish"
+  },
+  {
+    "title": "Counting by 7s"
+  },
+  {
+    "title": "Wait and See: Finding Peace in God's Pauses and Plans"
+  },
+  {
+    "title": "Competing Against Luck"
+  },
+  {
+    "title": "All Grown Up"
+  },
+  {
+    "title": "The Journal of Best Practices: A Memoir of Marriage, Asperger Syndrome, and One Man's Quest to Be a Better Husband"
+  },
+  {
+    "title": "Braving the Wilderness"
+  },
+  {
+    "title": "The Animators"
+  },
+  {
+    "title": "The Five People You Meet in Heaven"
+  },
+  {
+    "title": "Present Over Perfect"
+  },
+  {
+    "title": "Educated"
+  },
+  {
+    "title": "The Plant Paradox"
+  },
+  {
+    "title": "Finding God in the Waves"
+  },
+  {
+    "title": "Bad Blood Secrets and Lies in a Silicon valley startup"
+  },
+  {
+    "title": "The Magic Strings of Frankie Presto Bam Signed Ed"
+  },
+  {
+    "title": "I'm Just a Person"
+  },
+  {
+    "title": "Magic and Loss: The Internet as Art"
+  },
+  {
+    "title": "Thank You for Being Late: Pausing to Reflect on the Twenty-First Century"
+  },
+  {
+    "title": "The Subtle Art of Not Giving A F*Ck: A Counterintuitive Approach to Living a Good Life"
+  },
+  {
+    "title": "The Wonder"
+  },
+  {
+    "title": "The Fallback Plan"
+  },
+  {
+    "title": "Girl, Wash Your Face"
+  },
+  {
+    "title": "Pre-Suasion: A Revolutionary Way to Influence and Persuade"
+  },
+  {
+    "title": "Why Is God Laughing?: The Path to Joy and Spiritual Optimism"
+  },
+  {
+    "title": "Caucasia"
+  },
+  {
+    "title": "A Man Called Ove"
+  },
+  {
+    "title": "Emotional Design: Why We Love (or Hate) Everyday Things"
+  },
+  {
+    "title": "Rosie Project"
+  },
+  {
+    "title": "The Like Switch: An Ex-FBI Agent S Guide to Influencing, Attracting, and Winning People Over"
+  },
+  {
+    "title": "The Rosie Effect"
+  },
+  {
+    "title": "Mad About You"
+  },
+  {
+    "title": "Garden of Stones"
+  },
+  {
+    "title": "The Tattooist of Auschwitz"
+  },
+  {
+    "title": "Like Water for Chocolate"
+  },
+  {
+    "title": "All Our Wrong Todays"
+  },
+  {
+    "title": "Epic Measures: One Doctor. Seven Billion Patients."
+  },
+  {
+    "title": "Without Their Permission: How the 21st Century Will Be Made, Not Managed"
+  },
+  {
+    "title": "The Life-Changing Magic of Tidying Up: The Japanese Art of Decluttering and Organizing"
+  },
+  {
+    "title": "Illumination Night"
+  },
+  {
+    "title": "An Abundance of Katherines"
+  },
+  {
+    "title": "Growth Hacker Marketing: A Primer on the Future of PR, Marketing, and Advertising"
+  },
+  {
+    "title": "The Charisma Myth: How Anyone Can Master the Art and Science of Personal Magnetism"
+  },
+  {
+    "title": "The Nix"
+  },
+  {
+    "title": "Atomic Design"
+  },
+  {
+    "title": "The Heart"
+  },
+  {
+    "title": "If Disney Ran Your Hospital: 9 1/2 Things You Would Do Differently"
+  },
+  {
+    "title": "Alice I Have Been"
+  },
+  {
+    "title": "The Body Keeps the Score"
+  },
+  {
+    "title": "Making and Breaking the Grid: A Graphic Design Layout Workshop"
+  },
+  {
+    "title": "Eleanor Oliphant Is Completely Fine"
+  },
+  {
+    "title": "Asymmetry"
+  },
+  {
+    "title": "No Sweat: How the Simple Science of Motivation Can Bring You a Lifetime of Fitness"
+  },
+  {
+    "title": "Black Box Thinking"
+  },
+  {
+    "title": "If I Understood You, Would I Have This Look on My Face?: Relating to and Communicating with Others, from the Boardroom to the Bedroom"
+  },
+  {
+    "title": "Need to Know"
+  },
+  {
+    "title": "On The Verge"
+  },
+  {
+    "title": "How to Make Sense of Any Mess"
+  },
+  {
+    "title": "A Thousand Naked Strangers"
+  },
+  {
+    "title": "Super freakonomics"
+  },
+  {
+    "title": "The Invitation"
+  },
+  {
+    "title": "Exit West"
+  },
+  {
+    "title": "You Can't Touch My Hair"
+  },
+  {
+    "title": "How Not to Die"
+  },
+  {
+    "title": "The Case for God"
+  },
+  {
+    "title": "Hidden Figures: The Story of the African-American Women Who Helped Win the Space Race"
+  },
+  {
+    "title": "The Obstacle Is The Way"
+  },
+  {
+    "title": "Looking for Lovely: Collecting the Moments That Matter"
+  },
+  {
+    "title": "Me Before You"
+  },
+  {
+    "title": "Clients First"
+  },
+  {
+    "title": "Presence"
+  },
+  {
+    "title": "10% Happier: How I Tamed the Voice in My Head, Reduced Stress Without Losing My Edge, and Found Self-Help That Actually Works - A T"
+  },
+  {
+    "title": "Year of Yes: How to Dance It Out, Stand in the Sun and Be Your Own Person"
+  },
+  {
+    "title": "Grit"
+  },
+  {
+    "title": "A More Beautiful Question: The Power of Inquiry to Spark Breakthrough Ideas"
+  },
+  {
+    "title": "Originals"
+  },
+  {
+    "title": "Predictably Irrational"
+  },
+  {
+    "title": "The Best Interface Is No Interface: The Simple Path to Brilliant Technology"
+  },
+  {
+    "title": "The Storied Life of A. J. Fikry"
+  },
+  {
+    "title": "The Magic Strings of Frankie Presto"
+  },
+  {
+    "title": "Busy: How to Thrive in a World of Too Much"
+  },
+  {
+    "title": "When Work and Family Collide"
+  },
+  {
+    "title": "Accidental Saints: Finding God in All the Wrong People"
+  },
+  {
+    "title": "Children of Monsters: An Inquiry Into the Sons and Daughters of Dictators"
+  },
+  {
+    "title": "The Big Leap: Conquer Your Hidden Fear and Take Life to the Next Level"
+  },
+  {
+    "title": "The Search: How Google and Its Rivals Rewrote the Rules of Business and Transformed Our Culture"
+  },
+  {
+    "title": "The Invention of Wings"
+  },
+  {
+    "title": "The Immortal Life of Henrietta Lacks"
+  },
+  {
+    "title": "The Matchmaker"
+  },
+  {
+    "title": "The Things They Carried"
+  },
+  {
+    "title": "Scary Close"
+  },
+  {
+    "title": "Pastrix"
+  },
+  {
+    "title": "The Road to Character"
+  },
+  {
+    "title": "The Girl Who Saved the King of Sweden"
+  },
+  {
+    "title": "Crash the Chatterbox"
+  },
+  {
+    "title": "Being Mortal: Medicine and What Matters in the End"
+  },
+  {
+    "title": "Flow: The Psychology of Optimal Experience"
+  },
+  {
+    "title": "Ultra Metabolism"
+  },
+  {
+    "title": "The Best Yes: Making Wise Decisions in the Midst of Endless Demands"
+  },
+  {
+    "title": "Let's All Be Brave: Living Life with Everything You Have"
+  },
+  {
+    "title": "To Sell Is Human"
+  },
+  {
+    "title": "Drive: The Surprising Truth About What Motivates Us"
+  },
+  {
+    "title": "The Circle (Eggers novel)"
+  },
+  {
+    "title": "The Longest Ride"
+  },
+  {
+    "title": "The Hazards of Hunting While Heartbroken"
+  },
+  {
+    "title": "Daring Greatly: How the Courage to Be Vulnerable Transforms the Way We Live, Love, Parent, and Lead"
+  },
+  {
+    "title": "The Maze Runner"
+  },
+  {
+    "title": "If I Stay"
+  },
+  {
+    "title": "The Fault In Our Stars"
+  },
+  {
+    "title": "My Jesus Year"
+  },
+  {
+    "title": "The Biology of Good Luck"
+  },
+  {
+    "title": "Whistling Past the Graveyard"
+  },
+  {
+    "title": "American dervish : a novel"
+  },
+  {
+    "title": "Every Day"
+  },
+  {
+    "title": "Dash & Lily's Book of Dares"
+  },
+  {
+    "title": "The Five Love Languages"
+  },
+  {
+    "title": "This Is Where I Leave You"
+  },
+  {
+    "title": "A Long Way Down"
+  },
+  {
+    "title": "Wild: From Lost to Found on the Pacific Crest Trail"
+  },
+  {
+    "title": "They F*** You Up"
+  },
+  {
+    "title": "The Solitude of Prime Numbers"
+  },
+  {
+    "title": "I Know This Much Is True"
+  },
+  {
+    "title": "Lean In"
+  },
+  {
+    "title": "The Power of Habit: Why We Do What We Do in Life and Business"
+  },
+  {
+    "title": "Why We Get Fat: And What to Do About It"
+  },
+  {
+    "title": "It's not about the coffee : leadership principles from a life at Starbucks"
+  },
+  {
+    "title": "Unbearable Lightness: A Story of Loss and Gain"
+  },
+  {
+    "title": "The Shallows: What the Internet Is Doing to Our Brains"
+  },
+  {
+    "title": "Moonwalking with Einstein"
+  },
+  {
+    "title": "The Most Human Human: What Artificial Intelligence Teaches Us about Being Alive"
+  },
+  {
+    "title": "My Name Is Memory"
+  },
+  {
+    "title": "Quiet: The Power of Introverts in a World That Can't Stop Talking"
+  },
+  {
+    "title": "Father Fiction: Chapters for a Fatherless Generation"
+  },
+  {
+    "title": "What you must think of me : a firsthand account of one teenager's experience with social anxiety disorder"
+  },
+  {
+    "title": "The Fattening of America: How the Economy Makes Us Fat, If It Matters, and What to Do about It"
+  },
+  {
+    "title": "Does This Church Make Me Look Fat?: A Mennonite Finds Faith, Meets Mr. Right, and Solves Her Lady Problems"
+  },
+  {
+    "title": "The Magic Strings of Frankie Presto"
+  },
+  {
+    "title": "No Sweat: How the Simple Science of Motivation Can Bring You a Lifetime of Fitness"
+  },
+  {
+    "title": "Experience Design The Book"
+  },
+  {
+    "title": "Busy: How to Thrive in a World of Too Much"
+  },
+  {
+    "title": "Becoming a Supple Leopard by Kelly Starrett and Glen Cordoza"
+  }
+]
